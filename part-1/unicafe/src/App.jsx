2f38d7ac3b2908f396940ddad9d2747898e588c9
ABfathy <113472069+ABfathy@users.noreply.github.com>
2024-11-10 21:24:34 +0200

    finish step 1.7 of the unicafe exercise

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -12,25 +12,45 @@ const App = () => {
   const [good , setGood ] = useState(0);
   const [neutral , setNeutral ] = useState(0);
   const [bad, setBad ] = useState(0);
+  const [all , setAll] = useState(0);
+  const [average, setAverage ] = useState(0);
+  const [posFeedback, setPosFeedback] = useState(0);
 
   const handleGood = () =>{
 
     const newValue = good + 1;
+    const newAll = newValue + bad + neutral
+    const newAverage = (newValue - bad) / newAll
+    const newFeedback = (newValue / newAll) * 100
     setGood(newValue)
+    setAll(newAll)
+    setAverage(newAverage)
+    setPosFeedback(newFeedback)
 
   }
   const handleBad = () =>{
 
     const newValue = bad + 1;
+    const newAll = newValue + good + neutral
+    const newAverage = (good - newValue ) / newAll
+    const newFeedback = (good / newAll) * 100
     setBad(newValue)
+    setAll(newAll)
+    setAverage(newAverage)
+    setPosFeedback(newFeedback)
 
   }
   const handleNeutral = () =>{
 
     const newValue = neutral + 1;
+    const newAll = newValue + good + bad
+    const newFeedback = (good / newAll) * 100
     setNeutral(newValue)
+    setAll(newAll)
+    setPosFeedback(newFeedback)
 
   }
+
 
   return (
     <div>
@@ -42,6 +62,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p> 
+      <p>All {all}</p>
+      <p>Average {average} </p>
+      <p>Positive FeedBack : {posFeedback} </p>
 
     </div>
   )
