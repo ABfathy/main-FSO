558bd4829aaf1fb8a4429207cead6dec22dc12e1
ABfathy <113472069+ABfathy@users.noreply.github.com>
2024-11-12 13:18:45 +0200

    complete anecdote exercise

--- part-1/anecdotes/src/App.jsx
+++ part-1/anecdotes/src/App.jsx
@@ -1,5 +1,43 @@
 import { useState } from 'react'
 
+
+const Display = ({vote , anecdotes}) => {
+  let max = vote[0]
+  let curr = 0
+  let hasVoted = false
+  for(let i = 0 ; i < anecdotes.length;i++){
+
+    if(vote[i] > 0){
+      hasVoted = true
+    }
+
+    if(max < vote[i]){
+
+      max = vote[i]
+      curr = i
+      
+    
+    }
+
+  }
+  if(!hasVoted){
+
+    return ( <h1>No votes submitted yet</h1>)
+    
+  } 
+
+  return(
+
+    <>
+    <h1>Anecdote with the most votes numbered at {max}</h1>
+    <h2>{anecdotes[curr]}</h2>
+    </>
+
+  )    
+ 
+
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -11,22 +49,40 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+  
+  
+  const arr = Array(8).fill(0)
   const [selected, setSelected] = useState(0)
+  const [vote , setVote] = useState(arr)
+  
+  
 
   const handleClick = () =>{
 
     const rand = Math.floor(Math.random() * 8)
     setSelected(rand)
 
+
+  }
+
+  const handleVote = () => {
+
+    const copy = [...vote]
+    copy[selected]+=1
+    setVote(copy)
+  
   }
 
   return (
     <div>
-      <button onClick={handleClick}>Update quote</button>
-      <div>
+      <h1>
         {anecdotes[selected]}
-      </div>
+      </h1>
+      <h2>Has {vote[selected]} votes</h2>
+      <button onClick={handleClick}>Update quote</button>
+      <button onClick={handleVote}>vote</button>
+      <Display vote={vote} anecdotes={anecdotes} ></Display>
+      
     </div>
   )
 }
