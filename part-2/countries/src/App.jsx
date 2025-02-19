8b8e0921b3087f058a7465bbcccf6f3f7afc1a25
ABfathy <113472069+ABfathy@users.noreply.github.com>
2025-02-19 07:09:46 +0200

    feat: finished exercise 2.18

---
+++ part-2/countries/src/App.jsx
@@ -0,0 +1,70 @@
+import { useState , useEffect } from 'react'
+import axios from 'axios'
+
+
+function App() {
+  const [value, setValue] = useState('')
+  const [countries, setCountries] = useState([])
+  const [unfiltered, setUnfiltered ] = useState([])
+
+  
+  useEffect(()=>{
+
+    
+      axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => { 
+        setUnfiltered(response.data)
+      })
+
+  },[])
+  
+  useEffect(()=>{
+
+    if(unfiltered.length > 0){
+    
+      const searchList = unfiltered.filter( country  => country.name.common.toLowerCase().includes(value.toLowerCase()) )
+
+      setCountries(searchList) 
+    
+    }
+
+
+  },[value , unfiltered ])
+
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+
+  }
+
+
+  return (
+    <>
+    find countries <input type="text" value={value} onChange={handleChange}/>
+      <div>
+      {countries.length > 10 ? 'please specify your search more': 
+      countries.length === 1 ? countries.map( country =><div key={country.cca3}>
+
+        <h1>{country.name.official}</h1>
+        <p>{country.capital}</p>
+        <h1>Languages</h1>
+        <ul>
+        {Object.entries(country.languages).map(([code,language])=>(
+          <li key={`${country.cca3}-${code}`}> {language}</li>
+        )
+        )}
+        </ul>
+        <h1>Flag</h1>
+        <img src={country.flags.png} alt={`${country.name.common} flag`} width="500" />
+        </div>):
+        countries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+      }
+      </div>
+    </>
+    
+    
+  )
+}
+
+export default App
